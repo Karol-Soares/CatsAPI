094c1e5a9e2f3b028d9c300f1849b680341b1660
Natan Ramos <naelramosfilho@gmail.com>
2022-09-16 12:09:19 -0300

    Create tests for controller

---
+++ catsAPI/test/catsAPI_web/controllers/catsapi_controller_test.exs
@@ -0,0 +1,16 @@
+defmodule CatsApiTest  do
+  use CatsAPIWeb.ConnCase
+
+  describe "POST create" do
+    test "Success", %{conn: conn} do
+      cats_params = %{name: "Mel", age: 2, castrate: true, vacinated: true}
+
+      conn = post(conn, Routes.catsapi_path(conn, :create), cats_params)
+
+      assert response(conn, 201)
+      test "Error" , %{conn: conn} do
+
+      end
+    end
+  end
+end
