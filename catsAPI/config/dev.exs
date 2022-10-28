e67cda8bdae707e6065b8023c2e7f2eb17a13918
Natan5533 <naelramosfilho@gmail.com>
2022-10-28 15:00:52 -0300

    fix

--- catsAPI/config/dev.exs
+++ catsAPI/config/dev.exs
@@ -5,7 +5,7 @@ config :catsAPI, CatsAPI.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "catsapi_dev",
+  database: "catsapi",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
